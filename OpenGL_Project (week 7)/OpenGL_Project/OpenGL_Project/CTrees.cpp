31cf4feef46da371542e6c1d6a1234b4fcea00a2
Daniel West <daniel.west@mds.ac.nz>
2024-05-20 07:50:42 +1200

    fixed merge stuff

--- OpenGL_Project (week 7)/OpenGL_Project/OpenGL_Project/CTrees.cpp
+++ OpenGL_Project (week 7)/OpenGL_Project/OpenGL_Project/CTrees.cpp
@@ -14,7 +14,7 @@ CTrees::~CTrees()
 
 void CTrees::LoadTree()
 {
-	Tree = new CModel("Resources/Models/SM_Env_Tree_Palm_01.obj", m_InstancedMVPs);
+	Tree = new CModel("Resources/Models/SM_Env_Tree_Palm_01.obj");
 }
 
 void CTrees::Render(GLint _program, GLint _texture, glm::mat4 _matrix, float CurrentTime, glm::mat4 _projMat, glm::mat4 _viewMat)
@@ -27,20 +27,20 @@ void CTrees::Render(GLint _program, GLint _texture, glm::mat4 _matrix, float Cur
 	//	Tree->Render(_program, _texture, m_InstancedMVPs[i], CurrentTime, _projMat, _viewMat);
 	//}
 
-	Tree->RenderInstanced(_program, _texture, _matrix, CurrentTime, _projMat, _viewMat, m_InstancedMVPs);
+	//Tree->RenderInstanced(_program, _texture, _matrix, CurrentTime, _projMat, _viewMat, m_InstancedMVPs);
 
 }
 
 void CTrees::MakeMatrices()
 {
 	// randomize the position of trees
-	for (int i = 0; i < 100; i++)
-	{
-		// calculate model matrix
-		glm::mat4 TranslationMat = glm::translate(glm::mat4(1.0f), m_Position);
-		glm::mat4 RotationMat = glm::rotate(glm::mat4(1.0f), glm::radians((m_Rotation) * 10), glm::vec3(1.0f, 1.0f, 1.0f));
-		glm::mat4 ScaleMat = glm::scale(glm::mat4(1.0f), m_Scale);
-		// add new matrix to the vector
-		m_InstancedMVPs.push_back(TranslationMat * RotationMat * ScaleMat);
-	}
+	//for (int i = 0; i < 100; i++)
+	//{
+	//	// calculate model matrix
+	//	glm::mat4 TranslationMat = glm::translate(glm::mat4(1.0f), m_Position);
+	//	glm::mat4 RotationMat = glm::rotate(glm::mat4(1.0f), glm::radians((m_Rotation) * 10), glm::vec3(1.0f, 1.0f, 1.0f));
+	//	glm::mat4 ScaleMat = glm::scale(glm::mat4(1.0f), m_Scale);
+	//	// add new matrix to the vector
+	//	m_InstancedMVPs.push_back(TranslationMat * RotationMat * ScaleMat);
+	//}
 }
